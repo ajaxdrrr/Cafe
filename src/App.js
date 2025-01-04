97a2904231e900dd8c28a0168af7d7fbc62d5c69
ALVIN JEFFREY DELOS REYES <ajseyersoled25@gmail.com>
2025-01-04 19:27:19 +0800

    updated favicon and page title

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <img
           className="App-link"
-          href="https://reactjs.org"
+          src="https://firebasestorage.googleapis.com/v0/b/capstone-178d3.appspot.com/o/comfort-food%2FFINAL%20MENU%20-%20FINAL.png?alt=media&token=c6aa7c56-edbe-4d34-a476-35d41d0836dd"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        />
+        <img
+          className="App-link"
+          src="https://firebasestorage.googleapis.com/v0/b/capstone-178d3.appspot.com/o/comfort-food%2FFINAL%20MENU%20-%20FINAL%20(2).png?alt=media&token=8016ab7e-5fe2-4f44-b3d3-68c360eb2175"
+          target="_blank"
+          rel="noopener noreferrer"
+        />
       </header>
     </div>
   );
